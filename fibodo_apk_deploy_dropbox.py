c573ff504f8c68b51979c0f0e18c803c64aee43f
adrielad4 <adrielad4@gmail.com>
2019-09-06 16:43:13 +0530

    Update fibodo_apk_deploy_dropbox.py
    removed unused function

--- fibodo_apk_deploy_dropbox.py
+++ fibodo_apk_deploy_dropbox.py
@@ -6,59 +6,31 @@ import requests
 import json
 import re
 
-def get_app(release_dir):
-    '''Extract app data
-    
-    Args:
-        release_dir (str): Path to release directory.
-    Returns:
-        (str, str): App version and path to release apk file.
-    '''
-    output_path = os.path.join(release_dir, 'output.json')
-
-    with(open(output_path)) as app_output:
-        json_data = json.load(app_output)
-
-    apk_details_key = ''
-    if 'apkInfo' in json_data[0]:
-        apk_details_key = 'apkInfo'
-    elif 'apkData' in json_data[0]:
-        apk_details_key = 'apkData'
-    else:
-        print("Failed: parsing json in output file")
-        return None, None
-
-    app_version = json_data[0][apk_details_key]['versionName']
-    app_file = os.path.join(release_dir, json_data[0][apk_details_key]['outputFile'])
-    return app_version, app_file
-
 def upload_file(accesstoken, file_from, file_to):
-	
-	dbx = dropbox.Dropbox(accesstoken)
+
+    dbx = dropbox.Dropbox(accesstoken)
     
-    	for root, dirs, files in os.walk(file_from):
+        for root, dirs, files in os.walk(file_from):
 
-    		for filename in files:
+            for filename in files:
+                # construct the full local path
+                local_path = os.path.join(root, filename)
 
-        		# construct the full local path
-        		local_path = os.path.join(root, filename)
+                # construct the full Dropbox path
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                print(dropbox_path)
+                head, tail = os.path.split(dropbox_path)
+                print(head)
+                try:
+                        dbx.files_create_folder(head, False)
+                        print("created "+ head + " on dropbox")
+                except Exception as e:
+                        print("could not create dir with name", head)
+                #upload the file
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite)
 
-        		# construct the full Dropbox path
-        		relative_path = os.path.relpath(local_path, file_from)
-        		dropbox_path = os.path.join(file_to, relative_path)
-        		print(dropbox_path)
-				head, tail = os.path.split(dropbox_path)
-				print(head)
-        		try:
-            			dbx.files_create_folder(head, False)
-            			print("created "+ head + " on dropbox")
-         		except Exception as e:
-            			print("could not create dir with name", head)
-        		#upload the file
-        		with open(local_path, 'rb') as f:
-        			dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite)
-
-    
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--release.dir', dest='release_dir', help='path to release folder', required=True)
